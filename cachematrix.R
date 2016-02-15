c2cb11c011d4bd11e77a0325b4c8b7f65c3dfcca
Luca Tascedda <tax1977@gmail.com>
2016-02-15 16:06:35 +0100

    First solution to exercise

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  getMatrix <- function() x
+  getInverse <- function() inverse
+  setInverse <- function(inv) inverse <<-inv
+  setMatrix <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  } 
+  list(get=getMatrix,getInv=getInverse,setInv=setInverse,set=setMatrix)
 
 }
 
@@ -11,5 +20,20 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv = x$getInv()
+  if (!is.null(inv)) {
+    print("Inverse already calculated")
+    return(inv)
+  }
+  mat <- x$get()
+  if (det(mat)!=0) {
+    inv <- solve(x$get())
+  } 
+  else {
+    inv <- NULL
+    print("Matrix not invertible")
+  }
+  x$setInv(inv)
+  inv
+  
 }
